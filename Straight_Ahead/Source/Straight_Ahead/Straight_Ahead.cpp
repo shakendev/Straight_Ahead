17226923e34c9982719f9354b45926e5118eca9c
Dmitry Novikov <dimka22081998@icloud.com>
2021-05-16 19:13:02 +0300

    Straight Ahead [Game Project]
    Created:
    * New C++ project from Third Person template with default map and basic C++ classes

---
+++ Straight_Ahead/Source/Straight_Ahead/Straight_Ahead.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Straight_Ahead.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Straight_Ahead, "Straight_Ahead" );
+ 
---
+++ Straight_Ahead/Source/Straight_Ahead/Straight_AheadGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Straight_AheadGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStraight_AheadGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStraight_AheadGameMode();
+};
+
+
+
