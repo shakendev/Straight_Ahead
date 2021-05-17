f94badbb04e678dd94009991e4981daaa07361b3
Dmitry Novikov <dimka22081998@icloud.com>
2021-05-17 10:37:31 +0300

    Update [Game Project] files

---
+++ StraightAhead/Source/StraightAhead/StraightAhead.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class STRAIGHTAHEAD_API StraightAhead
+{
+public:
+	StraightAhead();
+	~StraightAhead();
+};
