6f4b14ff0cef26c9184719c59a6f361e1067b160
Dmitry Novikov <dimka22081998@icloud.com>
2021-09-03 13:12:46 +0300

    Create an empty C++ UE 4.27 Game Project

---
+++ Straight_Ahead/Source/Straight_Ahead/Straight_AheadGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Straight_AheadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRAIGHT_AHEAD_API AStraight_AheadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
